2ec37ca2d5ae8713a840ecdcbe987e166de8c7df
Narendra <narendraanjana09@gmail.com>
2021-09-08 15:07:49 +0530

    haavicavs

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
+const ref = firebase.database().ref("Entry");
+const storageRef = firebase.storage().ref("Muesum");
 
-const ref=firebase.database().ref('Entry');
-
-var val="";
-var entry={
-    name:"",
-    id:"",
-    des:""
+var val = "";
+var entry = {
+  name: "",
+  id: "",
+  des: "",
+  imgLink: "",
 };
 
-ref.on('value', (snapshot) => {
-    val="";
-    snapshot.forEach(function(childSnapshot) {
-         entry = childSnapshot.val();
-         val+=entry.name+"\n";
-       
-      });
-      console.log(val);
-      data.value=val;
-   
-   
+ref.on("value", (snapshot) => {
+  val = "";
+  var count = 0;
+  snapshot.forEach(function (childSnapshot) {
+    count++;
+    entry = childSnapshot.val();
+    val += count + ". " + entry.name + " " + entry.imgLink + "\n";
+    var img = document.createElement("img");
+
+    img.src = entry.imgLink;
+    img.style.width = "156px";
+    document.body.appendChild(img);
   });
-function upload(){
+  if (val == "") {
+    val = "no data";
+  }
+  console.log(val);
+  data.value = val;
+});
+function upload() {
+  var name = document.getElementById("itemName").value;
+  var id = document.getElementById("itemID").value;
+  var des = document.getElementById("itemDes").value;
+  const file = document.querySelector("#photo").files[0];
 
-      
-   var name=document.getElementById("itemName").value;
-   var id=document.getElementById("itemID").value;
-   var des=document.getElementById("itemDes").value;
-
-   if(name == "" || id == "" || des==""){
+  if (name == "" || id == "" || des == "" || file.name == "") {
     alert("enter value");
-   }else{
-      
-    
-   entry.name=name;
-   entry.id=id;
-   entry.des=des
-    ref.push(entry).then(function(data){
-       console.log("Item Submitted");
-    }).catch(function(error){
-        alert(error);
-        console.error(error);
-    })
-}
+  } else {
+    const fName = new Date() + "_" + file.name;
+
+    const metadata = {
+      contentType: file.type,
+    };
+
+    const task = storageRef.child(fName).put(file, metadata);
+
+    task
+      .then((snapshot) => snapshot.ref.getDownloadURL())
+      .then((url) => {
+        console.log(url);
+        entry.imgLink = url;
+        alert("Image Uploaded SuccessFullyy");
+        entry.name = name;
+        entry.id = id;
+        entry.des = des;
+        ref
+          .push(entry)
+          .then(function (data) {
+            alert("Item Submitted");
+          })
+          .catch(function (error) {
+            alert(error);
+            console.error(error);
+          });
+      });
+  }
 }
